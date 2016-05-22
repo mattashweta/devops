25bc079a9de4f42cbe66f67d47224de295193135
Shweta Matta <mattashweta@gmail.com>
2016-05-22 02:09:44 -0400

    Adding the Script for Edureka Devops Final Project

--- print_info.sh
+++ print_info.sh
@@ -1,3 +1,19 @@
 #!/bin/bash
-echo $USER
-echo $HOSTNAME
+echo "___________________________________________________________________________________________"
+echo "Printing the Info for the Project"
+echo "___________________________________________________________________________________________"
+echo " Various system configuration relates to user and OS"
+echo "User is " $USER
+echo "Hostname is " $HOSTNAME
+echo "OS is :" `uname`
+echo "___________________________________________________________________________________________"
+echo "Data related to load on the server."
+echo "Uptime and Load Average is " `uptime` 
+echo "___________________________________________________________________________________________"
+echo "Top 5 processes with ax number of threads"
+echo "___________________________________________________________________________________________"
+ps axo nlwp,pid,cmd | sort -rn | head -5
+echo "___________________________________________________________________________________________"
+echo "Sort the services by memory"
+echo "___________________________________________________________________________________________"
+ps aux --sort -rss | head -6
